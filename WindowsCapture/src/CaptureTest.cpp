37c5e752b9e8e59aa703cd1729c500afd5f53d06
Ctsdmm <1365710830@qq.com>
2023-05-01 19:58:42 +0800

    测试main文件

---
+++ WindowsCapture/src/CaptureTest.cpp
@@ -0,0 +1,27 @@
+﻿
+#include "CaptureBase/WindowsCaptureBase.h"
+
+
+#include <Windows.h>
+
+int main()
+{
+	WindowsCaptureBase cap;
+
+	HWND hwnd = FindWindow(NULL, L"坎特伯雷公主与骑士唤醒冠军之剑的奇幻冒险");
+
+	cap.SetCaptureTarget(hwnd);
+
+	while (true)
+	{
+		cv::Mat ma = cap.GetCaptureImage();
+		if (ma.data != NULL)
+		{
+			cv::imshow("1", ma);
+			cv::waitKey(1);
+		}
+	}
+
+
+	return 0;
+}
