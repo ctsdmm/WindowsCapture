fe3ba4e7d7a07e0942a8bdd1f1226570ed3665ca
Ctsdmm <1365710830@qq.com>
2023-05-01 19:18:41 +0800

    截屏基类头文件

--- WindowsCapture/src/CaptureBase/WindowsCaptureBase.h
+++ WindowsCapture/src/CaptureBase/WindowsCaptureBase.h
@@ -1,7 +1,6 @@
 ﻿#pragma once
 #include "direct3d11.interop.h"
 #include <wrl.h>
-#include <d3d11.h>
 #include <winrt/Windows.Graphics.DirectX.h>
 #include <winrt/Windows.Graphics.DirectX.Direct3d11.h>
 #include <winrt/Windows.Graphics.Capture.h>
@@ -10,11 +9,11 @@ class WindowsCaptureBase
 
 public:
 	bool Init();
-
+	bool SetCaptureTarget(const HWND& hwnd);
 private:
 	winrt::com_ptr<ID3D11Device> d3dDevice{ nullptr };
 	winrt::com_ptr< ID3D11DeviceContext>  d3dContext{ nullptr };
-
+	winrt::com_ptr<IDXGISwapChain1> m_swapChain{ nullptr };
 
 	winrt::Windows::Graphics::DirectX::Direct3D11::IDirect3DDevice device{};
 	winrt::Windows::Graphics::Capture::GraphicsCaptureItem m_item{ nullptr };
@@ -22,5 +21,4 @@ private:
 
 
 
-};
 
