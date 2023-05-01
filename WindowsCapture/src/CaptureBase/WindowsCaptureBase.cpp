400d14e807ca6ff909383f7695e4f5f134e25970
Ctsdmm <1365710830@qq.com>
2023-05-01 19:58:04 +0800

    桌面截图基类实现文件

--- WindowsCapture/src/CaptureBase/WindowsCaptureBase.cpp
+++ WindowsCapture/src/CaptureBase/WindowsCaptureBase.cpp
@@ -1,4 +1,5 @@
 ﻿#include "WindowsCaptureBase.h"
+#include "direct3d11.interop.h"
 
 bool WindowsCaptureBase::Init()
 {
@@ -15,5 +16,118 @@ bool WindowsCaptureBase::Init()
 		winrt::check_hresult(CreateDirect3D11DeviceFromDXGIDevice(dxgiDevice.get(), inspectable.put()));
 		device = inspectable.as<winrt::Windows::Graphics::DirectX::Direct3D11::IDirect3DDevice>();
 	}
-	// m_oclCtx = cv::directx::ocl::initializeContextFromD3D11Device(d3dDevice.get());
+	m_oclCtx = cv::directx::ocl::initializeContextFromD3D11Device(d3dDevice.get());
+	return true;
+}
+
+bool WindowsCaptureBase::SetCaptureTarget(const HWND& m_Hwnd)
+{
+	auto activation_factory = winrt::get_activation_factory<winrt::Windows::Graphics::Capture::GraphicsCaptureItem>();
+	auto interop_factory = activation_factory.as<IGraphicsCaptureItemInterop>();
+	interop_factory->CreateForWindow(m_Hwnd, winrt::guid_of<ABI::Windows::Graphics::Capture::IGraphicsCaptureItem>(), reinterpret_cast<void**>(winrt::put_abi(this->m_item)));
+	ItemSize = m_item.Size();
+	LastSize = ItemSize;
+	m_swapChain = CreateDXGISwapChain(
+		d3dDevice,
+		static_cast<uint32_t>(ItemSize.Width),
+		static_cast<uint32_t>(ItemSize.Height),
+		static_cast<DXGI_FORMAT>(winrt::Windows::Graphics::DirectX::DirectXPixelFormat::B8G8R8A8UIntNormalized),
+		2);
+
+	// Create framepool, define pixel format (DXGI_FORMAT_B8G8R8A8_UNORM), and frame size. 
+	m_framePool = winrt::Windows::Graphics::Capture::Direct3D11CaptureFramePool::CreateFreeThreaded(
+		device,
+		winrt::Windows::Graphics::DirectX::DirectXPixelFormat::B8G8R8A8UIntNormalized,
+		2,
+		ItemSize);
+	m_session = m_framePool.CreateCaptureSession(m_item);
+
+	m_frameArrived = m_framePool.FrameArrived(winrt::auto_revoke, { this, &WindowsCaptureBase::OnFrameArrived });
+	m_session.StartCapture();
+	return true;
+}
+
+cv::Mat WindowsCaptureBase::GetCaptureImage()
+{
+	std::lock_guard<std::mutex>lck(ImageMutex);
+	return resMat.clone();
+}
+
+void WindowsCaptureBase::OnFrameArrived(winrt::Windows::Graphics::Capture::Direct3D11CaptureFramePool const& sender, winrt::Windows::Foundation::IInspectable const& args)
+{
+
+	auto	t_start = std::chrono::steady_clock::now();
+	auto newSize = false;
+	{
+
+		auto frame = sender.TryGetNextFrame();
+		auto frameContentSize = frame.ContentSize();
+		if (frameContentSize.Width != LastSize.Width ||
+			frameContentSize.Height != LastSize.Height)
+		{
+
+			newSize = true;
+			LastSize = frameContentSize;
+			m_swapChain->ResizeBuffers(
+				2,
+				static_cast<uint32_t>(LastSize.Width),
+				static_cast<uint32_t>(LastSize.Height),
+				static_cast<DXGI_FORMAT>(winrt::Windows::Graphics::DirectX::DirectXPixelFormat::B8G8R8A8UIntNormalized),
+				0);
+		}
+
+		const auto surface = frame.Surface();
+		auto access = surface.as<IDirect3DDxgiInterfaceAccess>();
+		Microsoft::WRL::ComPtr< ID3D11Texture2D> texture;
+		const auto hr = access->GetInterface(winrt::guid_of<ID3D11Texture2D>(), (void**)texture.GetAddressOf());
+
+		if (SaveTexture2d_bool)
+		{
+
+			Microsoft::WRL::ComPtr< ID3D11Texture2D> des{ nullptr };
+			D3D11_TEXTURE2D_DESC texDesc;
+			texture->GetDesc(&texDesc);
+			texDesc.Usage = D3D11_USAGE_STAGING;
+			texDesc.BindFlags = 0;
+			texDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+			texDesc.MiscFlags = 0;
+			d3dDevice->CreateTexture2D(&texDesc, NULL, &des);
+
+			d3dContext->CopyResource(des.Get(), texture.Get());
+			if (des != nullptr)
+			{
+
+				std::lock_guard<std::mutex>lck(ImageMutex);
+
+				cv::directx::convertFromD3D11Texture2D(des.Get(), resMat);
+
+			}
+		}
+	}
+	if (m_swapChain != nullptr)
+	{
+		DXGI_PRESENT_PARAMETERS presentParameters = { 0 };
+		m_swapChain->Present1(1, 0, &presentParameters);
+	}
+
+
+
+	// 毫秒
+	auto t_end = std::chrono::steady_clock::now();
+	float time_span = std::chrono::duration<double, std::milli>(t_end - t_start).count();
+
+
+	if (time_span < TimeCur)
+	{
+		Sleep(TimeCur - time_span);
+	}
+	if (newSize)
+	{
+		m_framePool.Recreate(
+			device,
+			winrt::Windows::Graphics::DirectX::DirectXPixelFormat::B8G8R8A8UIntNormalized,
+			2,
+			LastSize);
+	}
+
 }
--- WindowsCapture/src/CaptureBase/WindowsCaptureBase.h
+++ WindowsCapture/src/CaptureBase/WindowsCaptureBase.h
@@ -1,24 +1,109 @@
 ﻿#pragma once
-#include "direct3d11.interop.h"
+
+
 #include <wrl.h>
+#include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Graphics.DirectX.h>
 #include <winrt/Windows.Graphics.DirectX.Direct3d11.h>
 #include <winrt/Windows.Graphics.Capture.h>
+#include <Windows.Graphics.Capture.Interop.h>
+#include <d3d11.h>
+#include <dxgi1_2.h>
+#include <mutex>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/directx.hpp>	
+
+
+
 class WindowsCaptureBase
 {
 
 public:
-	bool Init();
+	WindowsCaptureBase() { Init(); }
+
 	bool SetCaptureTarget(const HWND& hwnd);
+	void SetFps(const int& fps) { FPS = fps; TimeCur = 1000 / fps; }
+	cv::Mat GetCaptureImage();
 private:
+	bool Init();
+	// capture opencv mat;
+	cv::Mat  resMat;
+	cv::ocl::Context  m_oclCtx;
+
+	int FPS = 60;
+	float TimeCur = 1000.0 / 60;
+	//  set mutex   in   func"OnFrameArrived"  to help get cv::mat;
+	std::mutex ImageMutex;
+
+	//// set a bool to change the Directx2d source save
+	bool SaveTexture2d_bool = true;
+
+
+	// directx3d 
 	winrt::com_ptr<ID3D11Device> d3dDevice{ nullptr };
 	winrt::com_ptr< ID3D11DeviceContext>  d3dContext{ nullptr };
 	winrt::com_ptr<IDXGISwapChain1> m_swapChain{ nullptr };
 
 	winrt::Windows::Graphics::DirectX::Direct3D11::IDirect3DDevice device{};
+
+	//  WindowsCapture core
 	winrt::Windows::Graphics::Capture::GraphicsCaptureItem m_item{ nullptr };
 	winrt::Windows::Graphics::SizeInt32 ItemSize{};
+	winrt::Windows::Graphics::SizeInt32 LastSize{};
+	winrt::Windows::Graphics::Capture::Direct3D11CaptureFramePool m_framePool{ nullptr };
+	winrt::Windows::Graphics::Capture::Direct3D11CaptureFramePool::FrameArrived_revoker m_frameArrived;
+	winrt::Windows::Graphics::Capture::GraphicsCaptureSession m_session{ nullptr };
+
+	void OnFrameArrived(
+		winrt::Windows::Graphics::Capture::Direct3D11CaptureFramePool const& sender,
+		winrt::Windows::Foundation::IInspectable const& args);
+
+};
 
 
+
+inline auto
+CreateDXGISwapChain(
+	winrt::com_ptr<ID3D11Device> const& device,
+	const DXGI_SWAP_CHAIN_DESC1* desc)
+{
+	auto dxgiDevice = device.as<IDXGIDevice2>();
+	winrt::com_ptr<IDXGIAdapter> adapter;
+	winrt::check_hresult(dxgiDevice->GetParent(winrt::guid_of<IDXGIAdapter>(), adapter.put_void()));
+	winrt::com_ptr<IDXGIFactory2> factory;
+	winrt::check_hresult(adapter->GetParent(winrt::guid_of<IDXGIFactory2>(), factory.put_void()));
+
+	winrt::com_ptr<IDXGISwapChain1> swapchain;
+	winrt::check_hresult(factory->CreateSwapChainForComposition(
+		device.get(),
+		desc,
+		nullptr,
+		swapchain.put()));
+
+	return swapchain;
+}
+
+inline auto
+CreateDXGISwapChain(
+	winrt::com_ptr<ID3D11Device> const& device,
+	uint32_t width,
+	uint32_t height,
+	DXGI_FORMAT format,
+	uint32_t bufferCount)
+{
+	DXGI_SWAP_CHAIN_DESC1 desc = {};
+	desc.Width = width;
+	desc.Height = height;
+	desc.Format = format;
+	desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	desc.SampleDesc.Count = 1;
+	desc.SampleDesc.Quality = 0;
+	desc.BufferCount = bufferCount;
+	desc.Scaling = DXGI_SCALING_STRETCH;
+	desc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
+	desc.AlphaMode = DXGI_ALPHA_MODE_PREMULTIPLIED;
+
+	return CreateDXGISwapChain(device, &desc);
+}
 
 
