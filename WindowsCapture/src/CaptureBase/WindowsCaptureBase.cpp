f64e2533755ae70383c18cdb2ca7848d8b66d3d3
Ctsdmm <1365710830@qq.com>
2023-05-01 18:51:01 +0800

    first commit

---
+++ WindowsCapture/src/CaptureBase/WindowsCaptureBase.cpp
@@ -0,0 +1,19 @@
+﻿#include "WindowsCaptureBase.h"
+
+bool WindowsCaptureBase::Init()
+{
+	//Create Direct 3D Device
+	winrt::check_hresult(D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, D3D11_CREATE_DEVICE_BGRA_SUPPORT,
+		nullptr, 0, D3D11_SDK_VERSION, d3dDevice.put(), nullptr, nullptr));
+
+	d3dDevice->GetImmediateContext(d3dContext.put());
+	d3dContext->Release();
+
+	const auto dxgiDevice = d3dDevice.as<IDXGIDevice>();
+	{
+		winrt::com_ptr<::IInspectable> inspectable;
+		winrt::check_hresult(CreateDirect3D11DeviceFromDXGIDevice(dxgiDevice.get(), inspectable.put()));
+		device = inspectable.as<winrt::Windows::Graphics::DirectX::Direct3D11::IDirect3DDevice>();
+	}
+	// m_oclCtx = cv::directx::ocl::initializeContextFromD3D11Device(d3dDevice.get());
+}
---
+++ WindowsCapture/src/CaptureBase/WindowsCaptureBase.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "direct3d11.interop.h"
+#include <wrl.h>
+#include <d3d11.h>
+#include <winrt/Windows.Graphics.DirectX.h>
+#include <winrt/Windows.Graphics.DirectX.Direct3d11.h>
+#include <winrt/Windows.Graphics.Capture.h>
+class WindowsCaptureBase
+{
+
+public:
+	bool Init();
+
+private:
+	winrt::com_ptr<ID3D11Device> d3dDevice{ nullptr };
+	winrt::com_ptr< ID3D11DeviceContext>  d3dContext{ nullptr };
+
+
+	winrt::Windows::Graphics::DirectX::Direct3D11::IDirect3DDevice device{};
+	winrt::Windows::Graphics::Capture::GraphicsCaptureItem m_item{ nullptr };
+	winrt::Windows::Graphics::SizeInt32 ItemSize{};
+
+
+
+};
+
